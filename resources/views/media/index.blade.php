3f3df24a87357f82929dacbcb5e6446308f10c4f
Woenzelaar <112466769+Woenzelaar@users.noreply.github.com>
2024-05-13 15:08:56 +0200

    Update media index page to display photos with updated styling

--- resources/views/media/index.blade.php
+++ resources/views/media/index.blade.php
@@ -5,17 +5,18 @@
         </h2>
     </x-slot>
 
-    <section class="flex flex-wrap justify-around m-1 md:m-20">
-        @foreach ($photos as $photo)
-            <div class="bg-slate-200 rounded p-10 md:p-20 w-full md:w-1/2 mb-5 md:mb-0">
-                <img src="{{ asset($photo->image) }}" alt="{{ $photo->name }}" class="w-full h-48 object-cover object-center">
-                <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2 text-white">{{ $photo->name }}</div>
-                    <p class="text-white text-base">
-                        {{ $photo->description }}
-                    </p>
-                </div>
+    <section class="flex flex-wrap justify-around">
+    @foreach ($photos as $photo)
+        <div class="bg-slate-600 rounded p-2 w-1/4 m-4">
+            <img src="{{ asset($photo->image) }}" alt="{{ $photo->name }}" class="w-full h-48 object-cover object-center">
+            <div class="px-6 py-4">
+                <div class="font-bold text-xl text-white">{{ $photo->name }}</div>
+                <p class="text-white text-base">
+                    {{ $photo->description }}
+                </p>
             </div>
-        @endforeach
-    </section>
+        </div>
+    @endforeach
+</section>
+
 </x-app-layout>
